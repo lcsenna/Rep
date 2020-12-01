05f6e9b77c3d3fc3a8333658fc173907c4c91a17
lcsenna <U1319768402>
2020-12-01 07:51:26 -0300

    EP1

---
+++ EP1.py
@@ -0,0 +1,16 @@
+A= int(input())
+
+B= A%2
+C= A%3
+D= A%5
+
+
+if B == 0:
+ print("o número não é primo")
+elif C == 0:
+ print("o número não é primo")
+elif D==0:
+ print("o número não é primo")
+else :
+ print("o número é primo")
+
