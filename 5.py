955920706e857111d5177d20e2ef375ded709d70
lcsenna <74916783+lcsenna@users.noreply.github.com>
2021-01-18 16:24:48 -0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,27 @@
+class Circular(list):
+
+    def __init__(self):
+            list.__init__(self)
+            self.pos = None
+            pass
+    def corrente(self):
+        return self[self.pos]
+    def proximo(self):
+        if self.pos is None:
+            return None
+        if self.pos>=len(self)+1:
+                self.pos=0
+        else:
+            self.pos+=1
+        return self[self.pos]
+    def anterior(self):
+        if self.pos is None:
+            return None
+        if self.pos==0:
+            self.pos= len(self)-1
+        else:
+            self.pos-=1
+        return self[self.pos]
+    def insere(self,objeto):
+        self.append(objeto)
+        self.pos = len(self)-1
